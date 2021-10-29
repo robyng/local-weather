e4ee07e463124c01591e44c3aca2d694c6169724
Robyn Graham <robyngraham01@gmail.com>
2021-10-29 12:09:13 -0700

    icon link var and lis for 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,27 @@ searchForm.addEventListener("submit", inputHandler)
 
 
 var getCity = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a265ac1d58947ea1284e84b622179a3f"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=a265ac1d58947ea1284e84b622179a3f&units=imperial"
 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data)
 
             var cityNameEl = document.querySelector(".city-name");
-            cityNameEl.textContent = data.name
+            cityNameEl.textContent = data.city.name
+
+            var day1Date = document.querySelector("#day-1-date")
+            day1Date.textContent = data.list[0].dt_txt
+
+            var icon = document.querySelector(".weather-icon")
+            icon.textContent = data.list[0].weather[0].description
+
+            var iconImg = document.querySelector(".icon-img")
+            var iconCode = data.list[0].weather[0].icon
+            var iconLink = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+            iconImg.src = iconLink
+
+
         })
     })
 }
