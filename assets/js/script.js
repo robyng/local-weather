405a9666210e07e48e8f4fd13495539d2b736065
Robyn Graham <robyngraham01@gmail.com>
2021-10-31 19:06:29 -0700

    weather icon variables and restyle home page boostrap container fluid

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,26 +28,33 @@ var getCity = function(city) {
 
             var cityNameEl = document.querySelector(".city-name");
             cityNameEl.textContent = data.city.name
+            //var noon = data.list[3]
 
             var day1Date = document.querySelector("#day-1-date")
-            day1Date.textContent = data.list[0].dt_txt
+            day1Date.textContent = data.list[3].dt_txt
+
+            var day2Date = document.querySelector("#day-2-date")
+            day2Date.textContent = data.list[1].dt_txt
+
+            var day3Date = document.querySelector("#day-3-date")
+            day3Date.textContent = data.list[2].dt_txt
 
             var icon = document.querySelector(".weather-icon")
-            icon.textContent = data.list[0].weather[0].description
+            icon.textContent = data.list[5].weather[0].description
 
             var iconImg = document.querySelector(".icon-img")
-            var iconCode = data.list[0].weather[0].icon
+            var iconCode = data.list[5].weather[0].icon
             var iconLink = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
             iconImg.src = iconLink
 
             var temp = document.querySelector('.temp')
-            temp.textContent = data.list[0].main.temp
+            temp.textContent = data.list[5].main.temp
 
             var humidity = document.querySelector('.humidity')
-            humidity.textContent = data.list[0].main.humidity
+            humidity.textContent = data.list[5].main.humidity
 
             var wind = document.querySelector('.wind')
-            wind.TextContent = data.list[0].wind.speed
+            wind.textContent = data.list[5].wind.speed
 
         })
     })
@@ -63,3 +70,11 @@ var getCity = function(city) {
 //     cityNameEl.textContent = weatherResponse.name;
 
 // })
+
+// for (let i = 3; i < cars.length; i = i + 8) {
+//     text += cars[i] + "<br>";
+//     console.log(cars[3])
+//     console.log(cars[11])
+//     console.log(cars[19])
+    
+//   }
