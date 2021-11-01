aa6c6970997f3c0fd13e0d46ad623338df04b7db
robyng <robyngraham01@gmail.com>
2021-10-31 21:50:19 -0700

    add new fetch for current weather, add call to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,14 @@ var inputEl = document.querySelector("#search-city");
 
 var inputHandler = function(event) {
     event.preventDefault();
+    
+
     var cityChoice = inputEl.value.trim();
 
     if (cityChoice) {
-        getCity(cityChoice);
+        currentForcast(cityChoice)
+        forcast(cityChoice);
+        // resets search form to blank
         inputEl.value = "";
     } else {
         alert("No matches. please re-enter city name")
@@ -19,7 +23,48 @@ var inputHandler = function(event) {
 searchForm.addEventListener("submit", inputHandler)
 
 
-var getCity = function(city) {
+var currentForcast = function(city) {
+    var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a265ac1d58947ea1284e84b622179a3f&units=imperial"
+    fetch(currentApiUrl).then(function(response) {
+        response.json().then(function(cdata) {
+           console.log(cdata)
+
+            var currentWeatherEl = document.querySelector("#current-weather");
+            currentWeatherEl.textContent = cdata.name + "'s Current Weather"
+
+            var currentTempEl = document.querySelector(".current-temp");
+            currentTempEl.textContent = cdata.main.temp + " F*"
+
+            var currentWindEl = document.querySelector(".current-wind");
+            currentWindEl.textContent = cdata.wind.speed + " MPH"
+
+
+            // var day1Date = document.querySelector("#day-1-date")
+            // day1Date.textContent = data.list[3].dt_txt
+
+            // var icon = document.querySelector(".weather-icon")
+            // icon.textContent = data.list[3].weather[0].description
+
+            // var iconImg = document.querySelector(".icon-img")
+            // var iconCode = data.list[3].weather[0].icon
+            // var iconLink = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+            // iconImg.src = iconLink
+
+            // var temp = document.querySelector('.temp')
+            // temp.textContent = data.list[3].main.temp
+
+            // var humidity = document.querySelector('.humidity')
+            // humidity.textContent = data.list[3].main.humidity
+
+            // var wind = document.querySelector('.wind')
+            // wind.textContent = data.list[3].wind.speed
+
+        })
+    })
+}
+
+
+var forcast = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=a265ac1d58947ea1284e84b622179a3f&units=imperial"
 
     fetch(apiUrl).then(function(response) {
@@ -56,15 +101,6 @@ var getCity = function(city) {
 
 
 
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=san francisco&appid=a265ac1d58947ea1284e84b622179a3f')
-// .then(function(res){return res.json()})
-// //.then(function(data){console.log(data)})
-// //.catch(function(err){console.log(err)})
-// .then(function(weatherResponse){
-//     var cityNameEl = document.querySelector(".city-name");
-//     cityNameEl.textContent = weatherResponse.name;
-
-// })
 
 // for (let i = 3; i < cars.length; i = i + 8) {
 //     text += cars[i] + "<br>";
